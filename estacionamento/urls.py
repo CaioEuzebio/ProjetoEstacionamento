5452e2d862258b76bcd73b8748129d51241206e1
ceuzebio <caio.145@hotmail.com>
2020-01-05 15:50:51 -0300

    Add Menu

--- estacionamento/urls.py
+++ estacionamento/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    url(r'^core/', include('core.urls')),
+    url(r'^sistema/', include('core.urls')),
     url(r'^admin/', admin.site.urls),
 ]
