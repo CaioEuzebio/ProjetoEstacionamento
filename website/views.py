d04fa3114b23d4f1efeda3855427c4cbd07562ba
ceuzebio <caio.145@hotmail.com>
2020-01-07 22:47:01 -0300

    Bulding Contact Page

--- website/views.py
+++ website/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 def website_home(request):
     return render(request, 'website/index.html')
 
+def contato(request):
+    return render(request, 'website/contato.html')
+
 
