dda38c0c840e139fd57069d1ef1895c09d0f6dd5
ceuzebio <caio.145@hotmail.com>
2020-01-07 22:57:27 -0300

    Fiz social django bug

--- estacionamento/settings.py
+++ estacionamento/settings.py
@@ -19,7 +19,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'social_django',
     'bootstrapform',
     'website',
     'core',
