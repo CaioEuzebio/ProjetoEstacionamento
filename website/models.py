bf2549fdd4a95b463c96ad346291f83148ea3c3a
ceuzebio <caio.145@hotmail.com>
2020-01-10 00:34:28 -0300

    Saving Contact in database

--- website/models.py
+++ website/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Contato(models.Model):
+    email = models.EmailField()
+    nome = models.CharField(max_length=100, blank=False, null=False)
+    sobrenome = models.CharField(max_length=100, blank=False, null=False)
+    cidade = models.CharField(max_length=100, blank=False, null=False)
+    estado = models.CharField(max_length=100, blank=False, null=False)
+    mensagem = models.CharField(max_length=100, blank=False, null=False)
+    receber_promocoes = models.BooleanField()
+
+    def __str__(self):
+        return self.nome
+
+
+
+
+
