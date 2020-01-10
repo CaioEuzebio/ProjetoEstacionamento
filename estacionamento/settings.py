07cfc51788a14032b9858e762215da494db22e7a
Caio Euzebio <caio.145@hotmail.com>
2020-01-10 13:38:42 -0300

    Correct Settings.py

--- estacionamento/settings.py
+++ estacionamento/settings.py
@@ -10,7 +10,8 @@ DEBUG = config('DEBUG', default=False, cast=bool )
 
 
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'estacionamento24horas-app.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'estacionamento24h.herokuapp.com']
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
