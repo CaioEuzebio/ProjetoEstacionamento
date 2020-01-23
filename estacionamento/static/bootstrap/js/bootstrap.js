87dc0f1e1abf43a3b25bb9b1d8775f8d39a28b3f
Caio Euzebio <caio.145@hotmail.com>
2020-01-23 13:31:08 -0300

    Adding input masks

--- estacionamento/static/bootstrap/js/bootstrap.js
+++ estacionamento/static/bootstrap/js/bootstrap.js
@@ -3892,3 +3892,14 @@ Object.defineProperty(exports, '__esModule', { value: true });
 
 })));
 //# sourceMappingURL=bootstrap.js.map
+
+
+$(document).ready(function() {
+  $('.date-mask').mask('00/00/0000');
+  $('.date-mask-placeholder').mask('00/00/0000', {placeholder: "__/__/____"});
+  $('.phone-mask').mask('(00) 00000-0000');
+  $('.cpf-mask').mask('000.000.000-00');
+  $('.datepicker').datepicker({
+      dateFormat: "dd/mm/yy",
+  });
+});
